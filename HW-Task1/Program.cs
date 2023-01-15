ecd85cd0b1304fb8c6e687094eb0335f30391d1e
alexandra_rynkevich <alexandrar@playtika.com>
2023-01-15 21:57:33 +0100

    task1

---
+++ HW-Task1/Program.cs
@@ -0,0 +1,8 @@
+﻿// Enter a string from the console and output each character on a new line
+Console.WriteLine("Enter:");
+String userInput = Console.ReadLine();
+
+for (int i = 0; i < userInput.Length; i++)
+{
+    Console.WriteLine(userInput[i]);
+}
