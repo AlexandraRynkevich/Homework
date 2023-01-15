39af440c349936d711aa33c195b22c1240543110
alexandra_rynkevich <alexandrar@playtika.com>
2023-01-16 00:24:34 +0100

    task2

---
+++ HW-Task2/Program.cs
@@ -0,0 +1,24 @@
+﻿// Write a program to calculate the function x=12a2+7a+12. The parameter «a» is entered by the user. Provides validation of user input.
+using System.Security.Cryptography;
+using System.Xml.Linq;
+
+Console.WriteLine("x=12*(a*a)+7a+12");
+Console.WriteLine("Enter a=");
+string input = Console.ReadLine();
+
+int a;
+
+if (string.IsNullOrEmpty(input))
+{
+    Console.WriteLine("Value can't be empty! Enter a=");
+    input = Console.ReadLine();
+}
+while (!int.TryParse(input, out a))
+{
+    Console.WriteLine("This is not a number! Enter a=");
+    input = Console.ReadLine();
+}
+a = Convert.ToInt32(input);
+int x = 12 * (a * a) + 7 * a + 12;
+Console.WriteLine($"x={x}");
+
