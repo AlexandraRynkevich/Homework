b7fca21fb3cc38a0caa1cc5dff7561160434cf13
alexandra_rynkevich <alexandrar@playtika.com>
2023-02-19 20:49:06 +0100

    new task 6

---
+++ HW-Task6/Program.cs
@@ -0,0 +1,18 @@
+﻿//Given the radius of a circle and the side of a square. Which shape has more area?
+// S = π × r2
+// S = a × a = a2
+
+double radius = 2;
+double side = 2;
+double circle = radius * radius * Math.PI;
+double square = side * side;
+if (circle > square)
+    Console.WriteLine(" Circle " + "(" + circle + ")" + " has more area than " + " Square " + "(" + square + ")");
+else
+{
+    if (circle < square)
+        Console.WriteLine(" Square " + "(" + circle + ")" + " has more area than " + " Circle " + "(" + square + ")");
+    else
+        Console.WriteLine(" Circle " + "(" + circle + ")" + " is equal to " + " Square " + "(" + square + ")");
+}
+
