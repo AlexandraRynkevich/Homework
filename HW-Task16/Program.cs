55fecf3be424d20a5f6a2c30ffe277c3719fcb87
alexandra_rynkevich <alexandrar@playtika.com>
2023-04-07 15:15:44 +0200

    added new task 9 and 16

---
+++ HW-Task16/Program.cs
@@ -0,0 +1,42 @@
+﻿// Print numbers in the form of the following table:
+//a) 5
+//    5 5
+//    5 5 5
+//    5 5 5 5
+//    5 5 5 5 5
+
+//   10
+//   20 20
+//   30 30 30
+//   40 40 40 40
+//   50 50 50 50 50
+
+const int limit = 7;
+for (int i = 1; i <= limit; i++)
+{
+    Print(limit, i);
+}
+
+for (int i = limit; i > 0; i--)
+{
+    Print(1, i);
+}
+
+for (int i = 0; i <= limit; i++)
+{
+    Print(limit * 2 * i, i);
+}
+
+void Print(int x, int count)
+{
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write(x + " ");
+    }
+    Console.WriteLine();
+}
+
+for (int i = limit; i > 0; i--)
+{
+    Print((limit - i + 1) * limit, i);
+}
---
+++ HW-Task9/Program.cs
@@ -0,0 +1,45 @@
+﻿// Write a program that, depending on the ordinal number of the day of the week (1, 2, ..., 7), displays its name (Monday, Tuesday, ..., Sunday).
+
+Console.Write("Type a day number: ");
+int weekday = Convert.ToInt32(Console.ReadLine());
+
+while (weekday == 0)
+{
+    Console.Write("Incorrect number: ");
+    weekday = Convert.ToInt32(Console.ReadLine());
+}
+
+while (weekday >= 8)
+{
+    Console.Write("Incorrect number. Type a number from 1 to 7: ");
+    weekday = Convert.ToInt32(Console.ReadLine());
+}
+//weekday = Convert.ToInt32(Console.ReadLine()); 
+
+{
+    switch (weekday)
+    {
+        case 1:
+            Console.WriteLine("Monday");
+            break;
+        case 2:
+            Console.WriteLine("Tuesday");
+            break;
+        case 3:
+            Console.WriteLine("Wednesday");
+            break;
+        case 4:
+            Console.WriteLine("Thurthday");
+            break;
+        case 5:
+            Console.WriteLine("Friday");
+            break;
+        case 6:
+            Console.WriteLine("Saturday");
+            break;
+        case 7:
+            Console.WriteLine("Sunday");
+            break;
+    }
+}
+
